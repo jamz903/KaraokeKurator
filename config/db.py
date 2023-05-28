293d82d723e8887d3d96b1405a959f5ac02a259a
jamz903 <jamietohhl@gmail.com>
2023-05-28 19:09:08 +0800

    back to baseline

--- config/db.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-db_connection = MongoClient(process.env.MONGODB_URI)
-db = db_connection.database_name
-collection = db["posts"]
--- server.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-app = FastAPI()
-
-@app.get("/")
-async def home():   
-    return { "message": "Hello World"}
