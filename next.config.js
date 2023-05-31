1e61c0fcd57edb5a3b8536a124259aefa1ddd730
Mai Ting Kai <wheatgrass.mc@gmail.com>
2023-06-01 03:59:58 +0800

    Fix

--- next.config.js
+++ next.config.js
@@ -10,15 +10,6 @@ const nextConfig = {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api/:path*" : "/api/"
-      }
-    ];
   }
 };
 
