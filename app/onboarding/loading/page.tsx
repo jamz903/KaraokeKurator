e3c81ac6e3e7a0eb35acae3865f7f0a861ff8d26
Mai Ting Kai <wheatgrass.mc@gmail.com>
2023-05-31 23:57:42 +0800

    Get songs in playlist

--- app/onboarding/loading/page.tsx
+++ app/onboarding/loading/page.tsx
@@ -4,13 +4,34 @@ import ButtonPrimary from "app/components/common/buttons/button-primary";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { ScaleLoader } from "react-spinners";
+import { useEffect } from "react";
+import { useAppContext } from "app/AppContext";
 
 // REMOVE NEXT BUTTON
 // NAVIGATE TO `/playlists/output` AFTER RECOMMENDED SONGS LOAD FINISH
 
 const Playlist = () => {
   const searchParams = useSearchParams();
-  console.log(searchParams.get("playlistId"));
+  const playlistId = searchParams.get("playlistId");
+  const { authToken, changeAuthToken } = useAppContext();
+
+  useEffect(() => {
+    const token = authToken;
+    console.log(token);
+
+    fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    })
+      .then((response) => {
+        const res = response.json();
+        console.log(res);
+        return res;
+      })
+      .then((data) => console.log(data.items));
+  }, []);
+
   return (
     <Container className="flex flex-col align-center h-screen justify-between pb-[15vh]">
       <div className="flex flex-col items-center h-full pt-[15vh]">
